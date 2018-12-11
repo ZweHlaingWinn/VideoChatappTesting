693282eb3056a2260e75330fb7d19cd640f0becf
Zwe Hlaing Win <johnlast996@gmail.com>
2018-12-11 08:46:19 +0630

    first commit

---
+++ public/README.md
@@ -0,0 +1,16 @@
+#Simple webRTC Messager
+
+A tutorial on building a WebRTC video chat app.
+
+
+ 
+
+u can change port number in the server.js .
+
+
+
+npm start (or) node server
+
+
+
+URL in localhost:3000
---
+++ public/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+app.use((req, res)=> res.sendFile(`${__dirname}/public/index.html`));
+
+
+app.listen(port, (req, res)=>  console.info('listing on %d', port));
